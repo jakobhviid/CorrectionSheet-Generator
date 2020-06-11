6055bd6f0a718c6e9a3a97927936fe62e9c3d1d0
Jakob Hviid <jakobhviid1982@gmail.com>
2020-06-11 17:10:19 +0200

    created basic stucture

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+
+namespace CorrectionSheet_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("path\\to\\file.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {    
+                var records = csv.GetRecords<Foo>();
+            }
+        }
+    }
+}
