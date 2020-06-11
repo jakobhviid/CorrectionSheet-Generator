8ed3b2748be9633a16ab708533530c0bc3cc061b
Jakob Hviid <jakobhviid1982@gmail.com>
2020-06-11 18:34:10 +0200

    first working iteration

--- DigitalEksamenModel.cs
+++ DigitalEksamenModel.cs
@@ -1,9 +1,23 @@
 using System;
+using CsvHelper.Configuration.Attributes;
 
 namespace CorrectionSheet_Generator
 {
     class DigitalEksamenModel
     {
-        
+        [Index(0)]
+        public string StudentNumber { get; set; }
+        [Index(1)]
+        public string Name { get; set; }
+        [Index(2)]
+        public string Grade { get; set; }
+        [Index(3)]
+        public string OralExamStartDate { get; set; }
+        [Index(4)]
+        public string OralExamStartTime { get; set; }
+        [Index(5)]
+        public string OralExamEndTime { get; set; }
+        [Index(6)]
+        public string OralExamLocale { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Text;
+using System;
 using System.Globalization;
 using System.IO;
 using CsvHelper;
@@ -6,16 +7,60 @@ using CsvHelper;
 namespace CorrectionSheet_Generator {
     class Program {
         static void Main(string[] args) {
+            string encoding = "iso-8859-1"; // iso-8859-1
+
             // https://joshclose.github.io/CsvHelper/getting-started
-            // var reader = new StreamReader("path\\to\\file.csv");
-            // var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-            // var students = csv.GetRecords<DigitalEksamenModel>();
-            
-            // Write the string array to a new file named "WriteLines.txt".
+            var lines = new StreamReader(Path.Combine(Directory.GetCurrentDirectory(), "sample-studerende.csv"), Encoding.GetEncoding(encoding)).ReadToEnd().Replace("\"", String.Empty).Replace("=", String.Empty);
+            var preprocessedDataFile = new StreamWriter(Path.Combine(Directory.GetCurrentDirectory(), "preprocessed.csv"));
+            preprocessedDataFile.Write(lines);
+            preprocessedDataFile.Close();
+
+            var reader = new StreamReader(Path.Combine(Directory.GetCurrentDirectory(), "preprocessed.csv"), Encoding.GetEncoding(encoding));
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            csv.Configuration.MissingFieldFound = null;
+            csv.Configuration.Delimiter = ";";
+            csv.Configuration.Encoding = Encoding.GetEncoding(encoding);
+            var students = csv.GetRecords<DigitalEksamenModel>();
+
             using (StreamWriter outputFile = new StreamWriter(Path.Combine(Directory.GetCurrentDirectory(), "EVALUATION-DOCUMENT.md")))
             {
-                foreach (string student in students)
-                    outputFile.WriteLine("## test2");
+                outputFile.WriteLine("# Generated Evaluation Sheet for the DM and VOP Course");
+                outputFile.WriteLine("");
+                
+                foreach (var student in students) {
+                    outputFile.WriteLine("## " + student.Name + " with #StudentNumber " + student.StudentNumber);
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("### General Comments");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("### Assignment 1 - Subtask 2");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("- Bulletpoint here");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("### Assignment 1 - Subtask 2");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("- Bulletpoint here");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("### Assignment 1 - Subtask 2");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("- Bulletpoint here");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("### Assignment 1 - Subtask 2");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("- Bulletpoint here");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("### Assignment 1 - Subtask 2");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("- Bulletpoint here");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("### Assignment 1 - Subtask 2");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("- Bulletpoint here");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("### Final Assessment");
+                    outputFile.WriteLine("");
+                    outputFile.WriteLine("- Bulletpoint here");
+                    outputFile.WriteLine("");
+                }
             }
         }
     }
